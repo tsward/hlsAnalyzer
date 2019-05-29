561fc3cb554c5ca714da8170712a755ac5a9fa96
Tim Ward <tsy23@wildcats.unh.edu>
2019-05-29 03:46:40 -0400

    updated index.php

--- index.php
+++ index.php
@@ -11,41 +11,103 @@
 
             <?php
 
+
+
+
+    $dir = "segmented_videos/";
+    $videoW = 320;
+    $videoH = 240;
+
+    if (is_dir($dir))
+    {
+
+        if ($dh = opendir($dir)){
+            while (($file = readdir($dh)) !== false){
+
+                if($file != '.' && $file != '..'){
+                    echo 
+                    "
+                        <div style='display: block'>
+                            <video width=\"$videoW\" height=\"$videoH\" controls>
+                              <source src=\"". $dir . $file ."\" type=\"application/x-mpegURL\">
+                            </video>
+                        </div>
+                    ";
+
+                }
+
+            }
+
+            closedir($dh);
+
+          }
+          
+    }
+    
+
+
 /*
-$dir    = './segmented_videos/test';
+$dir    = './segmented_videos/demo';
 $video_files = [];
 
+
+$videos = array();
+$video_ext = array('m3u8');
+#foreach ($video_array as $path) {
+    foreach(glob($dir.'/*.*') as $path) {
+  if (in_array(pathinfo($path,  PATHINFO_EXTENSION), $video_ext)) {
+  //less general, but will work if you know videos always end in mp4
+  //if (pathinfo($path, PATHINFO_EXTENSION) == "mp4") {
+    $videos[] = $path;
+    print $path;
+  }
+}
+*/
+
+/*
 foreach(glob($dir.'/*.*') as $file) {
+    
     $file_parts = pathinfo($file);
     if ($file_parts['extension'] == "m3u8"){
-    #print "DUNKING BONERS" ;
+        $tmp = substr($file, strpos($file, "/") + 1);    
+        #print $tmp;
     #if($file_parts == "master.m3u8") {
-        #print "DUNKING BONERS: ";
-        #echo "<video autoplay>";
-        #echo "<source src=". $file ." type='video/mp4'>";
-        #echo "</video><br/>";
-        echo "<a href=\"https://www.w3schools.com/html/\">Visit our HTML tutorial</a>";
-        echo "<br>";
+        echo "<video autoplay>";
+        echo "<source src=". $file ." type='video/mp4'>";
+        echo "</video><br/>";
+        #echo "<a href=\"https://www.w3schools.com/html/\">Visit our HTML tutorial</a>";
+        #echo "<br>";
     }
 }
 */
 
-$dir    = './segmented_videos';
+
+/*
+$dir    = 'segmented_videos';
 $video_files = [];
 
-echo "<a href=\"segmented_videos/demo/master.m3u8\" style=\"font-size: 25px;\">Demo</a>";
+#echo "<a href=\"segmented_videos/demo/master.m3u8\" style=\"font-size: 25px;\">Demo</a>";
 
 
-
-foreach(glob($dir.'/*') as $boner) {
-    #print "$boner ";
-    #print $boner.$dir # http:// master.m3u8" +  "hey boner";
-    #echo "<a href=\"https://" + $dir + "/master.m3u8\">$boner</a>"; #"localhost/masters_project/hlsPlayer/segmented_videos/test\">$boner</a>";
+foreach(glob($dir.'/*') as $seg_file) {
     
-    #echo "<a href=\"masters_project/hlsPlayer/segmented_videos/test/master.m3u8\">$boner</a>";
+    $tmp = substr($seg_file, strpos($seg_file, "/") + 1);    
+    echo $tmp;
+    $eric = "\<a href=segmented_videos"; #demo/master.m3u8\" style=\"font-size: 25px;\">Demo</a>";
+    #$eric += "hi";
+    echo $eric;
+    #list($second) = explode("/", $seg_file);
+    # $seg_file = current(explode("/", $seg_file, 1));
+
+    #$tmp = strtok($seg_file, '/');
+    #print "$seg_file ";
+    #print $hi.$dir # http:// master.m3u8" +  "hey hi";
+    #echo "<a href=\"https://" + $dir + "/master.m3u8\">$hi</a>"; #"localhost/masters_project/hlsPlayer/segmented_videos/test\">$boner</a>";
+    
+    #echo "<a href=\"masters_project/hlsPlayer/segmented_videos/test/master.m3u8\">$hi</a>";
     #echo"<br>";
 }
-
+*/
 ?>
             
             <br><br><br><br>
